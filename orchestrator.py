179fb060f289f4af744f2fb063095accdb8e817e
Nawaljabeen <jabeennawal@gmail.com>
2026-05-01 20:22:37 +0000

    testing fixes for proportion and hallucinatin

--- orchestrator.py
+++ orchestrator.py
@@ -13,33 +13,34 @@ import mars_lib
 # 3. Trigger Execution
 test_payload = {
     "overall_bounding_box": {
-        "width_m": 0.45,
-        "depth_m": 0.32,
-        "height_m": 0.65
+        "width_m": 0.57,
+        "depth_m": 0.63,
+        "height_m": 0.99
     },
     "component_recipe": [
         {
-            "type": "surface",
-            "shape": "oval",
-            "is_cushion": False,
-            "z_position_ratio": 0.45,
-            "thickness_ratio": 0.03
-        },
-        {
             "type": "support",
             "count": 4,
-            "shape": "cylinder",
-            "pitch_angle": 7,
+            "shape": "box",
+            "thickness_m": 0.05
+        },
+        {
+            "type": "surface",
+            "shape": "square",
+            "is_cushion": False,
+            "z_position_ratio": 0.4,
+            "thickness_ratio": 0.05
+        },
+        {
+            "type": "armrest",
+            "is_cushion": False,
+            "z_position_ratio": 0.65,
             "thickness_m": 0.04
         },
         {
             "type": "backrest",
             "is_cushion": False,
-            "pitch_angle": 5,
-            "z_position_ratio": 0.7,
-            "thickness_m": 0.03,
-            "scale_w": 0.8,
-            "y_position": "back"
+            "thickness_m": 0.03
         }
     ]
 }
@@ -55,7 +56,7 @@ bpy.context.view_layer.update()
 output_dir = "/content/drive/MyDrive/MARS/output/"
 os.makedirs(output_dir, exist_ok=True)  # Safety net: creates the folder if it's missing
 
-output_path = os.path.join(output_dir, "mars_woodchair.glb")
+output_path = os.path.join(output_dir, "mars_ARMREST.glb")
 
 # Export the scene as a GLB
 bpy.ops.export_scene.gltf(filepath=output_path)
